992192ad99f007253712f7bf7e94a64c3a8df0e4
Ian Brun <crackedcomputerstudios@gmail.com>
2018-11-04 21:34:57 +0100

    Increased revolver shoot speed (as fast as the player can click)

--- src/game/server/hl2/weapon_357.cpp
+++ src/game/server/hl2/weapon_357.cpp
@@ -36,11 +36,19 @@ public:
 
 	void	PrimaryAttack( void );
 	void	Operator_HandleAnimEvent( animevent_t *pEvent, CBaseCombatCharacter *pOperator );
+#ifdef VANCE
+	void	ItemPostFrame( void );
+#endif
 
 	float	WeaponAutoAimScale()	{ return 0.6f; }
 
 	DECLARE_SERVERCLASS();
 	DECLARE_DATADESC();
+
+#ifdef VANCE
+private:
+	float	m_flSoonestPrimaryAttack;
+#endif
 };
 
 LINK_ENTITY_TO_CLASS( weapon_357, CWeapon357 );
@@ -51,6 +59,11 @@ IMPLEMENT_SERVERCLASS_ST( CWeapon357, DT_Weapon357 )
 END_SEND_TABLE()
 
 BEGIN_DATADESC( CWeapon357 )
+
+#ifdef VANCE
+	DEFINE_FIELD( m_flSoonestPrimaryAttack, FIELD_TIME ),
+#endif
+
 END_DATADESC()
 
 //-----------------------------------------------------------------------------
@@ -60,6 +73,10 @@ CWeapon357::CWeapon357( void )
 {
 	m_bReloadsSingly	= false;
 	m_bFiresUnderwater	= false;
+
+#ifdef VANCE
+	m_flSoonestPrimaryAttack = gpGlobals->curtime;
+#endif
 }
 
 //-----------------------------------------------------------------------------
@@ -158,3 +175,35 @@ void CWeapon357::PrimaryAttack( void )
 		pPlayer->SetSuitUpdate( "!HEV_AMO0", FALSE, 0 ); 
 	}
 }
+
+#ifdef VANCE
+//-----------------------------------------------------------------------------
+// Purpose: Allows firing as fast as button is pressed
+//-----------------------------------------------------------------------------
+void CWeapon357::ItemPostFrame(void)
+{
+	BaseClass::ItemPostFrame();
+
+	if ( m_bInReload )
+		return;
+	
+	CBasePlayer *pOwner = ToBasePlayer( GetOwner() );
+
+	if ( pOwner == NULL )
+		return;
+
+	// Allow a refire as fast as the player can click
+	if ( ( ( pOwner->m_nButtons & IN_ATTACK ) == false ) && ( (m_flSoonestPrimaryAttack + 0.1f) < gpGlobals->curtime ) )
+	{
+		m_flNextPrimaryAttack = gpGlobals->curtime - 0.1f;
+	}
+	else if ( ( pOwner->m_nButtons & IN_ATTACK ) && ( m_flNextPrimaryAttack < gpGlobals->curtime ) && ( m_iClip1 <= 0 ) )
+	{
+		WeaponSound(EMPTY);
+		SendWeaponAnim(ACT_VM_DRYFIRE);
+
+		m_flSoonestPrimaryAttack = gpGlobals->curtime + 0.2f;
+		m_flNextPrimaryAttack = gpGlobals->curtime + SequenceDuration();
+	}
+}
+#endif
