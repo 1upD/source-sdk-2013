1b0809d31c5abd2d17687ccdca0f19679270aba7
Ian Brun <crackedcomputerstudios@gmail.com>
2018-11-07 22:26:35 +0100

    Converted shotgun from pump action to semi-auto

--- src/game/server/hl2/weapon_shotgun.cpp
+++ src/game/server/hl2/weapon_shotgun.cpp
@@ -482,11 +482,19 @@ void CWeaponShotgun::PrimaryAttack( void )
 		pPlayer->SetSuitUpdate("!HEV_AMO0", FALSE, 0); 
 	}
 
-	if( m_iClip1 )
+#ifdef VANCE
+	//if( m_iClip1 )
+	//{
+	//	// pump so long as some rounds are left.
+	//	m_bNeedPump = true;
+	//}
+#else
+	if (m_iClip1)
 	{
 		// pump so long as some rounds are left.
 		m_bNeedPump = true;
 	}
+#endif
 
 	m_iPrimaryAttacks++;
 	gamestats->Event_WeaponFired( pPlayer, true, GetClassname() );
@@ -539,11 +547,19 @@ void CWeaponShotgun::SecondaryAttack( void )
 		pPlayer->SetSuitUpdate("!HEV_AMO0", FALSE, 0); 
 	}
 
-	if( m_iClip1 )
+#ifdef VANCE
+	//if( m_iClip1 )
+	//{
+	//	// pump so long as some rounds are left.
+	//	m_bNeedPump = true;
+	//}
+#else
+	if (m_iClip1)
 	{
 		// pump so long as some rounds are left.
 		m_bNeedPump = true;
 	}
+#endif
 
 	m_iSecondaryAttacks++;
 	gamestats->Event_WeaponFired( pPlayer, false, GetClassname() );
